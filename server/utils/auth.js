c130fb27d438b252c14ca56e16825524c8515ecf
Jack <jackeconomos@outlook.com>
2023-02-13 15:09:15 +1030

    fixed savebook

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -8,7 +8,7 @@ module.exports = {
   // function for our authenticated routes
   authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
-    let token = req.body.token || req.query.token || req.headers.authorization;
+    let token = req.query.token || req.headers.authorization || req.body.token;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -27,6 +27,9 @@ module.exports = {
       console.log('Invalid token');
       return res.status(400).json({ message: 'invalid token!' });
     }
+
+    // send to next endpoint
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
