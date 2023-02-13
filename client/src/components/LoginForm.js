e0b2fd25346c7e0801fe59fa083f1499d56abd11
Jack <jackeconomos@outlook.com>
2023-02-13 12:03:44 +1030

    updated forms

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,10 +1,9 @@
-// see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-import { useMutation } from '@apollo/react-hooks'
-import { LOGIN_USER } from '../utils/mutations';
 
 import Auth from '../utils/auth';
+import { useMutation } from '@apollo/react-hooks';
+import { LOGIN_USER } from '../utils/mutations';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
@@ -29,9 +28,10 @@ const LoginForm = () => {
 
     try {
       const { data } = await loginUser({
-        variables: { ...userFormData },
-    });
+        variables: {...userFormData} 
+      });
 
+      Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,22 +1,20 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+
+import Auth from '../utils/auth';
 import { useMutation } from '@apollo/react-hooks';
 import { ADD_USER } from '../utils/mutations';
-import Auth from '../utils/auth';
+
 
 const SignupForm = () => {
   // set initial form state
-  const [userFormData, setUserFormData] = useState({
-     username: '', 
-     email: '', 
-     password: '' 
-});
-
-  const [addUser, { error }] = useMutation(ADD_USER);
+  const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
   // set state for form validation
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+  // define mutation for adding a user
+  const [createUser] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -34,12 +32,11 @@ const SignupForm = () => {
     }
 
     try {
-      const { data } = await addUser({
-        variables: { ...userFormData },
-    });
+      const { data } = await createUser({
+        variables: { ...userFormData }
+      });
 
       Auth.login(data.addUser.token);
-
     } catch (err) {
       console.error(err);
       setShowAlert(true);
